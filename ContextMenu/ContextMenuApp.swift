6ba87763eed15272b9b94391ddb6e0f7298772a2
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-23 22:46:43 +0300

    Initial Commit

---
+++ ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuApp.swift
+//  ContextMenu
+//
+//  Created by Roman Fedotov on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
