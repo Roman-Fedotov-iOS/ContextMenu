c12606672012f3df541179fcb4803858ed748f0e
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-26 14:19:12 +0300

    1

--- ContextMenu/ContentView.swift
+++ ContextMenu/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Image("photo_2021-06-05 20.44.46")
+            .resizable()
+            .frame(height: 300)
+            .cornerRadius(10.0)
             .padding()
+            .contextMenu {
+                VStack {
+                    Button(action: {
+                        print("Copy")
+                    }) {
+                        HStack {
+                            Image(systemName: "doc.on.doc")
+                            Text("Copy")
+                        }
+                    }
+                    Button(action: {
+                        print("Share")
+                    }) {
+                        HStack {
+                            Image(systemName: "square.and.arrow.up")
+                            Text("Share")
+                        }
+                    }
+                }
+            }
     }
 }
 
